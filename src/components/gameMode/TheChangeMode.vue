0bda5d03e46ab3af465e2f12d2f02682285f3490
maty <matikster1@wp.pl>
2022-07-22 17:33:06 +0200

    Extension of TheChangeMode component with the possibility of clicking the mouse and adding a style

--- src/components/gameMode/TheChangeMode.vue
+++ src/components/gameMode/TheChangeMode.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="changeMode">
-    <p class="changeMode__pharagraf">Press SPACEBAR to change game mode.</p>
+    <p class="changeMode__pharagraf" @click="changeGameMode">
+      Press <span class="kbd">SPACEBAR</span> to change game mode or click on
+      the inscription.
+    </p>
   </div>
 </template>
 
@@ -25,6 +28,14 @@ export default defineComponent({
         }
       });
     });
+
+    function changeGameMode() {
+      if (!getFinish.value) {
+        changeModeName("");
+      }
+    }
+
+    return { changeGameMode };
   },
 });
 </script>
@@ -37,5 +48,20 @@ export default defineComponent({
 
 .changeMode__pharagraf {
   text-align: center;
+  cursor: pointer;
+}
+
+.kbd {
+  border: 1px solid gray;
+  font-size: 15px;
+  box-shadow: 1px 0 1px 0 #eee, 0 2px 0 2px #ccc, 0 2px 0 3px #444;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+  margin: 2px 3px;
+  padding: 1px 5px;
+  color: #000000;
+  font-weight: bold;
+  background: #eee;
 }
 </style>
